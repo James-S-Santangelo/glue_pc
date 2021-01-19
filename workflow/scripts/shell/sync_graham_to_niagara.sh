1c2e0f4925e772482eb1c6291ae4bfb49eeaa287
James Santangelo <james.santangelo37@@gmail.com>
2021-01-18 19:21:01 -0500

    Configuring SFS estimation to run on Niagara

--- workflow/scripts/shell/sync_graham_to_niagara.sh
+++ workflow/scripts/shell/sync_graham_to_niagara.sh
@@ -1,3 +1,2 @@
-rsync -vuar -P ../../../results/ \
-    --exclude '*.zarr/*'  \
-    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/toronto_gwsd/results
+rsync -vuar -P ../../../results/angsd/sfs/ \
+    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/toronto_gwsd/results/angsd/sfs
