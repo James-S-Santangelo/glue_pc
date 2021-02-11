4a3414d5f10e54775d23ed455d744169791e462e
James Santangelo <james.santangelo37@@gmail.com>
2021-02-11 11:13:45 -0500

    Syncing to Niagara

--- workflow/scripts/shell/sync_graham_to_niagara.sh
+++ workflow/scripts/shell/sync_graham_to_niagara.sh
@@ -1,2 +1,2 @@
-rsync -vuar -P ../../../results/angsd/sfs/ \
-    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/toronto_gwsd/results/angsd/sfs
+rsync -vuar -P ../../../results/bam \
+    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/glue-low1/snakemake/results/
