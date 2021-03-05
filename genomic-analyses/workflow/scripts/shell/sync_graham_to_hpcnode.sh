bef694323b2cf746eb40d5c8c6b631975eeba30c
James Santangelo <james.santangelo37@@gmail.com>
2021-03-05 10:39:51 -0500

    Minor changes to notebook

--- genomic-analyses/workflow/scripts/shell/sync_graham_to_hpcnode.sh
+++ genomic-analyses/workflow/scripts/shell/sync_graham_to_hpcnode.sh
@@ -1,2 +1,2 @@
-rsync -vuar -P santang3@gra-dtn1.computecanada.ca:/scratch/santang3/glue-low1/glue-paper1/genomic-analyses/results/ \
+rsync -vuar -P santang3@gra-dtn1.computecanada.ca:/scratch-deleted-2021-mar-20/santang3/glue-low1/glue-paper1/genomic-analyses/results/ \
     ../../../results
