b2389771bcc7275bdb4dd41106234d512887a695
James Santangelo <james.santangelo37@@gmail.com>
2021-03-05 16:55:11 -0500

    Squashed 'sequencing-prep/' changes from 81ab525..b318ffa
    b318ffa Adding 20 Toronto samples for sample sheet
    
    git-subtree-dir: sequencing-prep
    git-subtree-split: b318ffaece13794452d804bb42ea3b362bf3b9f8

--- scripts/data-processing/create_low1_sampleSheet.R
+++ scripts/data-processing/create_low1_sampleSheet.R
@@ -1,12 +1,16 @@
 # Script to create sample sheet for plants sequenced as part of LOW1
 
+# 20 samples from Toronto
+tor_samples <- read_csv('resources/toronto_samples_forGlue.csv')
+
 # Load in data and add native vs. introduced range
 low1_sampleSheet <- read_csv('data/clean/low1/low1_libraryConcentrations.csv') %>% 
   dplyr::select(continent, city, pop, individual, site, plantID) %>% 
   rename('sample' = 'plantID') %>% 
   mutate(range = case_when(continent == 'EU' ~ 'Native',
                           city == 'Tehran' ~ 'Native',
-                          TRUE ~ 'Introduced'))
+                          TRUE ~ 'Introduced')) %>% 
+  bind_rows(., tor_samples)
 
 outpath <- 'resources/'
 dir.create(outpath, showWarnings = FALSE)
