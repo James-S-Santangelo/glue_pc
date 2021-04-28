bc05b39549555f7b6999b746d976bc47c7e6cec1
James Santangelo <james.santangelo37@@gmail.com>
2021-04-28 16:27:23 -0400

    Working version of outlier sensitivity analysis

--- phenotypic-analyses/scripts/r/analyses/predictingClines.R
+++ phenotypic-analyses/scripts/r/analyses/predictingClines.R
@@ -14,60 +14,62 @@ df_all_popMeans <- c()
 for (i in 1:length(csv.files)){
   data <- read.csv(paste0(inpath, csv.files[i])) %>% dplyr::select(city, 
                                                                    std_distance, 
-                                                                   freqHCN, 
+                                                                   freqHCN,
+                                                                   total_plants,
                                                                    matches("*Mean$"))
   df_all_popMeans <- rbind(df_all_popMeans, data)
 }
 
-df_all_popMeans_excluded <- df_all_popMeans %>% 
-  dplyr::filter(!(city %in% c("St_Albert")))
+# df_all_popMeans_excluded <- df_all_popMeans %>% 
+#   dplyr::filter(!(city %in% c("St_Albert")))
 
 # Run function that returns matrix of slopes of environmental variables for each city
-results_statsMatrices <- calculate.stats(all.data = df_all_popMeans_excluded, 
+results_statsMatrices <- calculate.stats(all.data = df_all_popMeans, 
                                          permute = FALSE, 
                                          number.extreme.sites=2)
 
 # Extract slopes of environmental variables.
 envSlopes <- results_statsMatrices$slope.matrix %>% 
-  dplyr::select(-city) %>% 
-  setNames(names(.) %>% stringr::str_replace("Mean", "Slope")) %>% 
-  as.matrix()
+  setNames(names(.) %>% stringr::str_replace("Mean", "Slope")) 
 
 # Extract mean value of environmental variables for each city
-envMeans <- calculate_city_eviro_means(df_all_popMeans_excluded) %>% 
-  dplyr::select(-city) %>% 
+envMeans <- calculate_city_eviro_means(df_all_popMeans) %>% 
   drop_na() %>% 
-  setNames(paste0(names(.), "_Mean")) %>% 
-  as.matrix()
+  rename_if(is.numeric, paste0, "_Mean")
 
-# Get Slopes of HCN clines from Robust regression
-HCNslopes <- slope.freqHCN(all.data = df_all_popMeans_excluded, number.extreme.sites=1)
-HCNslopes <- HCNslopes$slopes %>% 
-  as.data.frame() %>% 
-  dplyr::select(rlm) %>%
-  rename("HCNslope" = "rlm") %>% 
-  as.matrix()
+# Get change in log-odds of HCN from Robust regression
+logOdds <- df_all_popMeans %>% 
+  group_split(city) %>% 
+  map_df(., logistic_regression_stats) %>% 
+  dplyr::select(city, betaLog) %>% 
+  filter(city %in% envSlopes$city)  # Only cities with environmental data
 
 # Distance vector: Distance between urban and rural multivariate environments for each city
 distance_vector <- results_statsMatrices$D.UR
 
 # Create dataframe with HCN slopes and environmental data
-df_slopes_enviro <- data.frame(HCNslopes, envSlopes, envMeans)
+df_slopes_enviro <- left_join(logOdds, envSlopes, by = 'city') %>% 
+  left_join(., envMeans, by = 'city')
+
+# Convert to matrices
+logOdds_mat <- df_slopes_enviro %>% dplyr::select(betaLog) %>% as.matrix()
+envSlopes_mat <- df_slopes_enviro %>% dplyr::select(contains('_Slope')) %>% as.matrix()
+envMeans_mat <- df_slopes_enviro %>% dplyr::select(contains('_Mean')) %>% as.matrix()
 
 ####################################################
 #### CORRELATION AMONG ENVIRONMENTAL PREDICTORS ####
 ####################################################
 
 # Create correlation matrix
-envSlopes_corr <- generate_correlation_df(envSlopes)
-envMeans_corr <- generate_correlation_df(envMeans)
+envSlopes_corr <- generate_correlation_df(envSlopes_mat)
+envMeans_corr <- generate_correlation_df(envMeans_mat)
 
 # Create the plots
-pairs(envMeans, 
+pairs(envMeans_mat, 
       lower.panel = panel.cor,
       upper.panel = upper.panel)
 
-pairs(envSlopes, 
+pairs(envSlopes_mat, 
       lower.panel = panel.cor,
       upper.panel = upper.panel)
 
@@ -75,29 +77,34 @@ pairs(envSlopes,
 #### ENVIRONMENTAL PREDICTORS OF HCN CLINES ####
 ################################################
 
-# Create Matrix with predictors (including interactions)
-# Do not include interactions involving distance vector
-# Do not include predictors with interactions among mean environmental variables
-predictors <- cbind(distance_vector, envSlopes, envMeans)
-predictors_withInteractions <- model.matrix( ~.^2, data = data.frame(scale(predictors)))[,-1]  # remove intercept
-predictors_withInteractions_reduced <- predictors_withInteractions %>% 
+# Create scaled predictor model matrix, including 2-way interactions
+# Include only interactions between slopes of 2 environmental variables,
+# and between means and slopes of 2 variables.
+predictors <- cbind(distance_vector, envSlopes_mat, envMeans_mat) %>% 
   as.data.frame() %>% 
-  dplyr::select(-contains(':'),  # Remove al interaction
+  mutate_if(is.numeric, scale)
+
+predictors_withInteractions <- model.matrix( ~.^2, data = predictors) %>% 
+  as.data.frame() %>% 
+  dplyr::select(-'(Intercept)') %>% 
+  
+  # Format interactions
+  dplyr::select(-contains(':'),  # Remove all interaction
                 contains('Slope:')) %>% 
   as.matrix()
 
-# Combine predictors with HCN slopes
-model_matrix <- cbind(HCNslopes, predictors_withInteractions_reduced)
+# Combine predictors with log odds into final model matrix
+model_matrix <- cbind(logOdds_mat, predictors_withInteractions)
 
 # Run Elastic Net model selection
 set.seed(1)
 elasticNet_model <- caret::train(
-  y = model_matrix[,1], # HCNslopes
+  y = model_matrix[,1], # Log Odds
   x = model_matrix[,-1], # All predictores
   method = "glmnet",
   metric = "RMSE",
   trControl = trainControl("cv", number = 10, savePredictions = "all"),
-  tuneLength = 10
+  tuneLength = 25
 )
 
 # Full list of results
@@ -112,17 +119,104 @@ bestLambda <- elasticNet_bestTune$lambda
 EN_finalModel <- elasticNet_model$finalModel
 
 # Extract nonzero coefficients
-coef(EN_finalModel, bestLambda)
-predictors_nonZero <- which(coef(EN_finalModel, bestLambda)!=0)[2:length(which(coef(EN_finalModel, bestLambda)!=0))]-1
-predictors_finalModel <- predictors_withInteractions_reduced[, predictors_nonZero]
+EN_final_modelCoefs <- coef(EN_finalModel, bestLambda)
+predictors_nonZero <- which(EN_final_modelCoefs!=0)[2:length(which(EN_final_modelCoefs!=0))]-1
+predictors_finalModel <- predictors_withInteractions[, predictors_nonZero]
 
 # Create data frame for model. Add back in main effects that are not in model
-model_df_elasticNet = as.data.frame(cbind(HCNslopes, predictors_finalModel))
+model_df_elasticNet = as.data.frame(cbind(logOdds_mat, predictors_finalModel))
 
 # Run final model 
-predClines_elasticNet <- lm(HCNslopes ~ ., data = model_df_elasticNet[-1])
+predClines_elasticNet <- lm(betaLog ~ ., data = model_df_elasticNet)
+
+# Diagnostics. A couple outliers
+plot(predClines_elasticNet)
+hist(residuals(predClines_elasticNet))
+
+# Model summary
 predClines_elasticNet_summary <- summary(predClines_elasticNet)
 predClines_elasticNet_anova <- Anova(predClines_elasticNet, type = 3)
+
+# What are the outliers?
+df_slopes_enviro %>% 
+  bind_cols(., residuals(predClines_elasticNet) %>% as.tibble()) %>% 
+  filter(value < -5 | value > 5) %>% 
+  dplyr::select(city)
+  
+############################################
+#### OUTLIER SENSITIVITY OF ELASTIC NET ####
+############################################
+
+## Remove Tampa and Shanghai 
+
+# Model matrix without Tampa and Shanghai
+model_matrix_noTamp_noShang <- model_matrix %>% 
+  cbind(., df_slopes_enviro %>% dplyr::select(city)) %>% 
+  filter(!(city %in% c('Shanghai'))) %>% 
+  dplyr::select(-city)
+
+predictors_withInteractions_noTamp_noShang <- predictors_withInteractions %>% 
+  cbind(., df_slopes_enviro %>% dplyr::select(city)) %>% 
+  filter(!(city %in% c('Shanghai'))) %>% 
+  dplyr::select(-city) %>% 
+  as.matrix()
+
+logOdds_mat_noTamp_noShang <- logOdds_mat %>% 
+  cbind(., df_slopes_enviro %>% dplyr::select(city)) %>% 
+  filter(!(city %in% c('Shanghai'))) %>% 
+  dplyr::select(-city) %>% 
+  as.matrix()
+
+# Run Elastic Net model selection
+set.seed(1)
+elasticNet_model_noTamp_noShang <- caret::train(
+  y = model_matrix_noTamp_noShang[,1], # Log Odds
+  x = model_matrix_noTamp_noShang[,-1], # All predictores
+  method = "glmnet",
+  metric = "RMSE",
+  trControl = trainControl("cv", number = 10, savePredictions = "all"),
+  tuneLength = 25
+)
+
+# Full list of results
+elasticNet_modelResults_noTamp_noShang <- data.frame(elasticNet_model_noTamp_noShang$results)
+
+# Best tuning parameter
+elasticNet_bestTune_noTamp_noShang <- elasticNet_model_noTamp_noShang$bestTune
+bestAlpha_noTamp_noShang <- elasticNet_bestTune_noTamp_noShang$alpha
+bestLambda_noTamp_noShang <- elasticNet_bestTune_noTamp_noShang$lambda
+
+# Final model
+EN_finalModel_noTamp_noShang <- elasticNet_model_noTamp_noShang$finalModel
+
+# Extract nonzero coefficients
+EN_final_modelCoefs_noTamp_noShang <- coef(EN_finalModel_noTamp_noShang, bestLambda_noTamp_noShang)
+predictors_nonZero_noTamp_noShang <- which(EN_final_modelCoefs_noTamp_noShang!=0)[2:length(which(EN_final_modelCoefs_noTamp_noShang!=0))]-1
+predictors_finalModel_noTamp_noShang <- predictors_withInteractions_noTamp_noShang[, predictors_nonZero_noTamp_noShang]
+
+# Create data frame for model. Add back in main effects that are not in model
+model_df_elasticNet_noTamp_noShang = as.data.frame(cbind(logOdds_mat_noTamp_noShang, predictors_finalModel_noTamp_noShang))
+
+# Run final model 
+predClines_elasticNet_noTamp_noShang <- lm(betaLog ~ ., data = model_df_elasticNet_noTamp_noShang)
+
+# Diagnostics. A couple outliers
+plot(predClines_elasticNet_noTamp_noShang)
+hist(residuals(predClines_elasticNet_noTamp_noShang))
+
+# Model summary
+predClines_elasticNet_summary_noTamp_noShang <- summary(predClines_elasticNet_noTamp_noShang)
+predClines_elasticNet_anova_noTamp_noShang <- Anova(predClines_elasticNet_noTamp_noShang, type = 3)
+
+
+
+
+
+
+
+##############################################
+#### ADD MAIN EFFECTS BACK TO ELASTIC NET ####
+##############################################
 
 # Run final model with main effects back in the model.
 # Main effects are required to get predicted effects for interactions (Hierarchy principle)
