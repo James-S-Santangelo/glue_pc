34d2b0e61577b2819a69a07daf4e8ffbde9ad5a6
James Santangelo <james.santangelo37@@gmail.com>
2021-05-04 22:33:42 -0400

    Changin paths in script to sync files to Niagara

--- genomic-analyses/workflow/scripts/shell/sync_graham_to_niagara.sh
+++ genomic-analyses/workflow/scripts/shell/sync_graham_to_niagara.sh
@@ -1,2 +1,9 @@
-rsync -vuar -P ../../../results/program_resources \
-    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/glue-low1/snakemake/results/
+rsync -vuar -P \
+    --exclude 'qc' \
+    --exclude 'figures' \
+    --exclude 'merged' \
+    --exclude 'paired' \
+    --exclude 'unpaired' \
+    --exclude 'final/s_*' \
+    ../../../results \
+    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/glue/glue-paper1/genomic-analyses
