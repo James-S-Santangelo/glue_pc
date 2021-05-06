2354fefd6486386c94e6db591b1143e3bab9f271
James Santangelo <james.santangelo37@@gmail.com>
2021-05-05 20:09:47 -0400

    Updating script to syn niagara to graham

--- genomic-analyses/workflow/scripts/shell/sync_niagara_to_graham.sh
+++ genomic-analyses/workflow/scripts/shell/sync_niagara_to_graham.sh
@@ -1,2 +1,14 @@
-rsync -vuar -P santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/glue-low1/glue-paper1/genomic-analyses/results/angsd \
-    ../../../results
+rsync -vuar -P \
+    --dry-run \
+    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/glue/glue-paper1/genomic-analyses/workflow/logs \
+    ../../logs/
+
+rsync -vuar -P \
+    --dry-run \
+    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/glue/glue-paper1/genomic-analyses/workflow/slurm_logs \
+    ../../slurm_logs/
+
+rsync -vuar -P \
+    --dry-run \
+    santang3@nia-datamover1.scinet.utoronto.ca:/scratch/n/nessrobe/santang3/glue/glue-paper1/genomic-analyses/results/angsd \
+    ../../../results/angsd/
