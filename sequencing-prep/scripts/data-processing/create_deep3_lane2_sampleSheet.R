2a023bf30cad58c26c5be997f3dd36f45a7578ef
James Santangelo <james.santangelo37@gmail.com>
2021-07-06 20:51:32 -0400

    Fix to bugs in sample names for Lane 2

--- sequencing-prep/scripts/data-processing/create_deep3_lane2_sampleSheet.R
+++ sequencing-prep/scripts/data-processing/create_deep3_lane2_sampleSheet.R
@@ -1,13 +1,22 @@
 # Script to create sample sheet for plants sequenced as part of DEEP3, LANE2
 
 # Load in data and add native vs. introduced range
+isdup <- function (x) duplicated (x)
 deep3_lane2_sampleSheet <- read_csv('data/clean/deep3/deep3_lane2_dilutions.csv') %>% 
 
   dplyr::select(continent, city, pop, individual, site, plantID, lane, i5_primer_index_seq, i7_primer_index_seq) %>% 
   rename('sample' = 'plantID') %>% 
   mutate(range = case_when(continent == 'EU' ~ 'Native',
                            city == 'Tehran' ~ 'Native',
-                           TRUE ~ 'Introduced'))
+                           TRUE ~ 'Introduced')) %>% 
+  
+  # Shorten Palmerston North sample IDs since they're too long for Novogene Sample Sheet
+  mutate(sample = str_replace(sample, '^Palmerston_North', 'Palm_North')) %>% 
+  
+  # Add '_dup_' suffix to duplicated samples
+  mutate(is_dup = isdup(sample),
+         sample = ifelse(is_dup, paste0(sample, '_dup'), sample)) %>% 
+  dplyr::select(-is_dup)
   
 
 outpath <- 'resources/'
