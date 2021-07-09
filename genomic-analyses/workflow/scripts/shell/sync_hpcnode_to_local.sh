b7089355738f2fc5f236b9ca25e1ec1e14860624
James Santangelo <james.santangelo37@gmail.com>
2021-07-09 16:47:18 -0400

    Change to script to sync figures

--- genomic-analyses/workflow/scripts/shell/sync_hpcnode_to_local.sh
+++ genomic-analyses/workflow/scripts/shell/sync_hpcnode_to_local.sh
@@ -1,28 +1,4 @@
 # ANGSD
 rsync -vuar -P \
-    --prune-empty-dirs \
-    --include='*/' \
-    --include='*allChroms.sfs' \
-    --include='*01.1_sum*' \
-    --include='*GL*_baq*.sfs' \
-    --include='allSamples*.pestPG' \
-    --exclude='*' \
-    santang3@hpcnode1.utm.utoronto.ca:/scratch/research/projects/trifolium/toronto_gwsd/results/angsd/ \
-    ../../../data
-
-rsync -vuar -P \
-    --prune-empty-dirs \
-    --include='*/' \
-    --include='*.cov' \
-    --include='*seed1.qopt' \
-    --exclude='*' \
-    santang3@hpcnode1.utm.utoronto.ca:/scratch/research/projects/trifolium/toronto_gwsd/results/population_structure/ \
-    ../../../data
-
-rsync -vuar -P \
-    --prune-empty-dirs \
-    --include='*/' \
-    --include='angsd_sample_order.txt' \
-    --exclude='*' \
-    santang3@hpcnode1.utm.utoronto.ca:/scratch/research/projects/trifolium/toronto_gwsd/results/program_resources/ \
-    ../../../data
+    santang3@hpcnode1.utm.utoronto.ca:/research/projects/trifolium/glue-paper1/genomic-analyses/results/figures/ \
+    ../../../results
